68faf70007cd141a290f7cdb6326c860ccf05066
mailo1709 <mailoheller77@icloud.com>
2026-07-27 21:30:57 +0000

    Deploy build to GitHub Pages

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/kingdome-wolde-v3/',
-  plugins: [react()],
-})
